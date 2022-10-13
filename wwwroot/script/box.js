ef3b46951992ca875ceb8a2dca43778f651bf582
DiogoPedro <dpfs@cin.ufpe.br>
2022-10-13 08:35:41 -0300

    Mudancas de scripts

--- wwwroot/script/box.js
+++ wwwroot/script/box.js
@@ -1,19 +1,19 @@
-window.changeColorID = (nameID, color) => {
-    let element = document.getElementById("nameID");
-    element.style.color = color;
-}
+// window.changeColorID = (nameID, color) => {
+//     let element = document.getElementById("nameID");
+//     element.style.color = color;
+// }
 
-window.changeColorClass = (data) => {
-    let nameClass = data.Classe;
-    let color = data.Color;
+// window.changeColorClass = (data) => {
+//     let nameClass = data.Classe;
+//     let color = data.Color;
 
-    alert(data);
+//     alert(data);
 
-    let element = document.getElementsByTagName(nameClass)[0];
-    //element.style.color = color + "!important;";
-}
+//     let element = document.getElementsByTagName(nameClass)[0];
+//     //element.style.color = color + "!important;";
+// }
 
-window.changeContext = (nameID, context) => {
-    let element = document.getElementById(nameID);
-    element.innerHTML = context;
-}
+// window.changeContext = (nameID, context) => {
+//     let element = document.getElementById(nameID);
+//     element.innerHTML = context;
+// }
--- wwwroot/script/simples.js
+++ wwwroot/script/simples.js
@@ -3,20 +3,24 @@ window.showMessageConsole = (message) => {
     return true;
 }
 
-window.alertAadsa = function (message) {
-    alert(message + "como passa");
+// window.alertAadsa = function (message) {
+//     alert(message + "como passa");
+// }
+
+function teste() {
+    alert("Oi");
 }
 
 window.squareJS = (x) => {
     return x * x;
 }
 
-window.changeName = (x) => { 
+window.changeName = (variavel) => { 
     const x = document.getElementsByClassName('box')[0];
     //let x = document.getElementsByClassName("box");
     //window.alert(x.innerHTML + "");
 
     //const x = document.getElementById('box');
-    x.innerHTML = '<h2>'+ x + '</h2>';
+    x.innerHTML = '<h2>'+ variavel + '</h2>';
 
 }
